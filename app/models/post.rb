ba8b2ce457a53c5e162a83c0bd63bcbcc2d83379
CodedBeardedSignedTaylor <thetaylorjones@gmail.com>
2014-05-29 15:22:15 -0500

    Implemented Comment System

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { maximum: 50 }
 	validates :text, presence: true, length: { minimum: 5 }
 end
