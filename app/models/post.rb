5e35164755132ae3c621f34ace4c634bcc453373
CodedBeardedSignedTaylor <thetaylorjones@gmail.com>
2014-06-11 16:21:57 -0500

    Reworked Admin Framework. Enhanced Authentication.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
-	# Comment implementation - Saved for later. 
-	# has_many :comments, dependent: :destroy
+	# Comment implementation - Saved for later.
+	belongs_to :admin
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { maximum: 50 }
 	validates :text, presence: true, length: { minimum: 5 }
 end
