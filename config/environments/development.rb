0516ba4f4b0abe7f6636bfdfc25d8666f9119f0f
CodedBeardedSignedTaylor <thetaylorjones@gmail.com>
2014-08-26 11:47:42 -0500

    Security patch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,13 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['collateral-development'],
+      :access_key_id => ENV['AKIAIYX4BYEBE2EF33UA'],
+      :secret_access_key => ENV['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']
+    }
+  }
 end
