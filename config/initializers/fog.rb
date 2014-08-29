a5ddf66ec6524ff2fa6642b1bc2864917af7bf20
CodedBeardedSignedTaylor <thetaylorjones@gmail.com>
2014-08-29 12:35:22 -0500

    Heroku fixes

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,7 +1,23 @@
 CarrierWave.configure do |config|
-  if Rails.env.development?
-    S3_ENV = YAML.load_file("#{::Rails.root}/config/aws_cred.yml")[Rails.env]	 
+  # if Rails.env.development?
+  #   S3_ENV = YAML.load_file("#{::Rails.root}/config/aws_cred.yml")[Rails.env]	 
 
+  #   config.fog_credentials = {
+  #     # Configuration for Amazon S3 should be made available through an Environment variable.
+  #     # For local installations, export the env variable through the shell OR
+  #     # if using Passenger, set an Apache environment variable.
+  #     #
+  #     # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
+  #     #
+  #     # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
+   
+  #     # Configuration for Amazon S3
+  #     :provider              => S3_ENV["provider"],
+  #     :aws_access_key_id     => S3_ENV["access_key_id"],
+  #     :aws_secret_access_key => S3_ENV["secret_access_key"]
+  #   }
+
+  # else
     config.fog_credentials = {
       # Configuration for Amazon S3 should be made available through an Environment variable.
       # For local installations, export the env variable through the shell OR
@@ -12,25 +28,9 @@ CarrierWave.configure do |config|
       # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
    
       # Configuration for Amazon S3
-      :provider              => S3_ENV["provider"],
-      :aws_access_key_id     => S3_ENV["access_key_id"],
-      :aws_secret_access_key => S3_ENV["secret_access_key"]
-    }
-
-  else
-    config.fog_credentials = {
-      # Configuration for Amazon S3 should be made available through an Environment variable.
-      # For local installations, export the env variable through the shell OR
-      # if using Passenger, set an Apache environment variable.
-      #
-      # In Heroku, follow http://devcenter.heroku.com/articles/config-vars
-      #
-      # $ heroku config:add S3_KEY=your_s3_access_key S3_SECRET=your_s3_secret S3_REGION=eu-west-1 S3_ASSET_URL=http://assets.example.com/ S3_BUCKET_NAME=s3_bucket/folder
-   
-      # Configuration for Amazon S3
-      :provider              => S3_ENV["provider"],
-      :aws_access_key_id     => S3_ENV["access_key_id"],
-      :aws_secret_access_key => S3_ENV["secret_access_key"]
+      :provider              => ENV["provider"],
+      :aws_access_key_id     => ENV["access_key_id"],
+      :aws_secret_access_key => ENV["secret_access_key"]
     }
   end
 
