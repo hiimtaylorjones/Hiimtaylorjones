85243440dc0bbd4cbef00e0f362b57e5cce2da77
CodedBeardedSignedTaylor <thetaylorjones@gmail.com>
2014-08-29 10:46:14 -0500

    Fixed Edit Profile Bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up)        { |u| u.permit(:avatar, :username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in)        { |u| u.permit(:username, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:avatar) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:avatar, :current_password) }
   end
 end
