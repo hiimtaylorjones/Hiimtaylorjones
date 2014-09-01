69cc16fdc0de328e4d88af141fda582ed4d9e73b
CodedBeardedSignedTaylor <thetaylorjones@gmail.com>
2014-09-01 12:01:54 -0500

    Heroku fixes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_everythingisruby_sessions'
+Rails.application.config.session_store :cookie_store, key: '_everythingisruby_session'
