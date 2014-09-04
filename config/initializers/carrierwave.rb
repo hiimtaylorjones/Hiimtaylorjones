cfe6f9ea9dbe6e8f9e13af282b0c9cb37f35119b
CodedBeardedSignedTaylor <thetaylorjones@gmail.com>
2014-09-04 14:12:55 -0500

    Heroku Fixes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,15 +12,13 @@ CarrierWave.configure do |config|
 
   # For production, AKA Heroku.
   else
-    config.storage    = :aws
-    config.aws_bucket = ENV['S3_BUCKET_NAME']
-    config.aws_acl    = :public_read
-    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
-
-    config.aws_credentials = {
-      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    config.fog_credentials = { 
+      :provider               => 'AWS', 
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'], 
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'], 
     }
+    config.fog_public     = false 
+    config.fog_directory = ENV['S3_BUCKET_NAME']
   end
  
   config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
