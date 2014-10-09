642883a858d54f99bf4ef67aeddab512b6b9a404
CodedBeardedSignedTaylor <taylorjonessoftware@gmail.com>
2014-10-09 10:19:38 -0500

    CSS Fixes

--- app/uploaders/title_image_uploader.rb
+++ app/uploaders/title_image_uploader.rb
@@ -22,7 +22,7 @@ class TitleImageUploader < CarrierWave::Uploader::Base
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   
     #{}"/images/fallback/" + [version_name, "default.png"].compact.join('_')
-    'atl.jpg' #rails will look at 'app/assets/images/default_avatar.png'
+    'atl.jpg'#rails will look at 'app/assets/images/default_avatar.png'
   end
 
   # Process files as they are uploaded:
