176a500657bdda19b9a6ff21e45807c7b46bc63a
CodedBeardedSignedTaylor <taylorjonessoftware@gmail.com>
2015-03-03 23:31:57 -0600

    validation fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { maximum: 50 }
 	validates :text, presence: true, length: { minimum: 5 }
-	validates :draft, presence: true
 	mount_uploader :title_image, TitleImageUploader
 end
