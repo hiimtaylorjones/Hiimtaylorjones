a06512dfe44bae12545758fd4bfd4cdbfcb5af70
CodedBeardedSignedTaylor <tjones@motionmobs.com>
2015-11-15 13:06:04 -0600

    more changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,7 +20,7 @@
 set :stage, :production
 
 # Replace 127.0.0.1 with your server's IP address!
-server '104.131.110.47', user: 'root', roles: %w{web app}
+server '104.131.210.65', user: 'root', roles: %w{web app}
 
 set :application, 'EverythingIsRuby'
 set :repo_url, 'git@github.com:codedbeardedsignedtaylor/everythingisruby.git'
