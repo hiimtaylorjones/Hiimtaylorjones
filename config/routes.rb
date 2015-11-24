a9bccad79cb4fdba024081b92281733eb32e6d40
CodedBeardedSignedTaylor <tjones@motionmobs.com>
2015-11-23 21:26:30 -0600

    routes rescue

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   match '/admin',       to: 'basic_page#admin',         via: 'get'
   match '/hire_me',     to: 'inquiries#new',            via: 'get'
   match '/blog',        to: 'posts#index',              via: 'get'
+
+  get '*path' => redirect { |p, req| 
+    req.flash[:notice] = "#{p[:path]} isn't a page on this site. Sorry!";
+   '/'
+  }
 end
