3de9e462a4b0deba685906bb78597bff8b3b0e49
CodedBeardedSignedTaylor <taylorjonessoftware@gmail.com>
2015-11-29 19:16:57 -0600

    why

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,16 +14,13 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.assets.compile = true
-  config.assets.precompile =  ['*.js', '*.css', '*.css.erb', '*css.less', '*.js.coffee']
-
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_files = true
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
