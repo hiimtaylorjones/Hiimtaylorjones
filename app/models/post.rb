295d2d5034bead967c31c9b2c7b3263884d16c34
CodedBeardedSignedTaylor <taylorjonessoftware@gmail.com>
2016-02-10 14:00:57 -0600

    integrated friendly_id

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ActiveRecord::Base
+
+	extend FriendlyId
+	friendly_id :title, use: :slugged
+
 	has_many :comments, dependent: :destroy
+
 	validates :title, presence: true, length: { maximum: 150 }
 	validates :body, presence: true, length: { minimum: 5 }
+
 	acts_as_taggable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126162603) do
+ActiveRecord::Schema.define(version: 20160210195255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,19 @@ ActiveRecord::Schema.define(version: 20160126162603) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+
   create_table "inquiries", force: :cascade do |t|
     t.string   "name"
     t.string   "company"
@@ -71,7 +84,10 @@ ActiveRecord::Schema.define(version: 20160126162603) do
     t.string   "tagline"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "posts", ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
 
   create_table "responses", force: :cascade do |t|
     t.integer  "comment_id"
