243344e0587d914dd1274efa75a5b55c15ba63fd
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-05-28 22:47:17 -0400

    cleaning out the assets things

--- app/assets/javascripts/basic_page.js
+++ app/assets/javascripts/basic_page.js
@@ -3,10 +3,10 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on('ready page:load', function() {
- var navbar = document.getElementsByClassName("nav");
+ var navbar = document.getElementsByClassName("nav")[0];
  var color = ["#4CAF50", "#03A9F4", "#f34d4d", "#f765b8","#ffab7f", "#35c390"];
  var ind = Math.floor(Math.random() * color.length);
- navbar[0].style['background-color'] = color[ind];
+ navbar.style['background-color'] = color[ind];
 
  $.each($('.code'), function(i, block) {
    hljs.highlightBlock(block);
