4b1753d63a500a8fcfdcf21c0f4e414bdd7bea56
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-07-04 20:57:16 -0400

    switching out markdown elements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
 	require 'github/markup'
 
 	def markdown(text)
-		GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, text)
+		GitHub::Markup.render_s(GitHub::Markups::MARKUP_MARKDOWN, text).html_safe
 	end
 end
