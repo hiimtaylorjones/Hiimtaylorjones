281941867225e1a0f9e21a2e79b7b324b02671f2
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-07-04 21:27:03 -0400

    ui updates; bug in posts controller

--- app/assets/javascripts/basic_page.js
+++ app/assets/javascripts/basic_page.js
@@ -3,11 +3,6 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
 $(document).on('ready page:load', function() {
- var navbar = document.getElementsByClassName("nav")[0];
- var color = ["#4CAF50", "#03A9F4", "#f34d4d", "#f765b8","#ffab7f", "#35c390"];
- var ind = Math.floor(Math.random() * color.length);
- navbar.style['background-color'] = color[ind];
-
  $.each($('.code'), function(i, block) {
    hljs.highlightBlock(block);
  });
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,6 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
