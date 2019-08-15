e1d0f655259efb758acb6ee1dd730753ffd9d784
Taylor Jones <taylor.jones@heroku.com>
2019-08-15 16:02:48 -0400

    cleaning up stylesheets; adding in better tag functionality

--- app/controllers/basic_page_controller.rb
+++ app/controllers/basic_page_controller.rb
@@ -2,7 +2,7 @@ class BasicPageController < ApplicationController
 
   def index
     @posts = Post.where(published: true)
-    @posts = @posts.order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
+    @posts = @posts.order('created_at DESC').paginate(:page => params[:page], :per_page => 1)
   end
 
   def about
