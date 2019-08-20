ca5cb4ec79556ad7abe03af278a7229b22b11641
Taylor Jones <taylor.jones@heroku.com>
2019-08-20 12:12:03 -0400

    Getting Post Rating component to conditionally render feedback forms based on a rating

--- app/javascript/packs/PostRating.js
+++ app/javascript/packs/PostRating.js
@@ -6,6 +6,41 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import './styles/post-rating'
 
+function RatingButton(props) {
+  return (
+    <button name="rating" value={props.value} onClick={props.onClick}>{props.value}</button>
+  );
+}
+
+function PositiveFeedbackForm(props) {
+  return (
+    <div className="feedback-form">
+      <h4>Why did you enjoy this article?</h4>
+      <textarea className="feedback-text"></textarea>
+    </div>
+  );
+}
+
+function NegativeFeedbackForm(props) {
+  return (
+    <div className="feedback-form">
+      <h4>What could this article have done better?</h4>
+      <textarea className="feedback-text"></textarea>
+    </div>
+  );
+}
+
+
+
+function AverageFeedbackForm(props) {
+  return (
+    <div className="feedback-form">
+      <h4>What about this article seems bland?</h4>
+      <textarea></textarea>
+    </div>
+  );
+}
+
 class PostRating extends Component {
   constructor(props) {
     super(props);
@@ -13,24 +48,41 @@ class PostRating extends Component {
       rating: "",
       email: ""
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.setRating = this.setRating.bind(this);
   };
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
+  setRating(event) {
+    this.setState({ rating: event.target.value });
+    console.log(this.state);
   };
   
   render() {
+    let feedbackForm;
+    const rating = this.state.rating;
+
+    if (["1", "2"].includes(rating)){
+      feedbackForm = <NegativeFeedbackForm />;
+    } else if (rating === "3") {
+      feedbackForm = <AverageFeedbackForm />;
+    } else if (["4", "5"].includes(rating)) {
+      feedbackForm = <PositiveFeedbackForm />;
+    }
+
     return(
-      <div className="rating-form">
-        <h3>So, what'd you think of this post?</h3>
-        <div className="input-group">
-          <label>Give it a Rating out of 5</label><br />
-          <button name="time" value="1" onClick={this.handleChange}>1</button>
-          <button name="time" value="2" onClick={this.handleChange}>2</button>
-          <button name="time" value="3" onClick={this.handleChange}>3</button>
-          <button name="time" value="4" onClick={this.handleChange}>4</button>
-          <button name="time" value="5" onClick={this.handleChange}>5</button>
+      <div>
+        <div className="rating-form">
+          <h3>So, what'd you think of this post?</h3>
+          <div className="input-group">
+            <label>Rate it on a scale of 1 to 5</label><br />
+            <RatingButton value="1" onClick={this.setRating} />
+            <RatingButton value="2" onClick={this.setRating} />
+            <RatingButton value="3" onClick={this.setRating} />
+            <RatingButton value="4" onClick={this.setRating} />
+            <RatingButton value="5" onClick={this.setRating} />
+          </div>
+        </div>
+        <div className="feedback-form">
+          {feedbackForm}
         </div>
       </div>
     );
