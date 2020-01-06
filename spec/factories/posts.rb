de711b5beb476072e795c6250892071df115310e
Taylor Jones <taylor.jones@heroku.com>
2020-01-06 15:33:40 -0500

    Updating factories; working with post views

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,10 +1,18 @@
 require 'faker'
 
+def generate_post_body
+	paragraphs = []
+	15.times do
+		paragraphs << Faker::Lorem.paragraph(sentence_count: 5)
+	end
+	return paragraphs.join(" ")
+end
+
 FactoryBot.define do
 	factory :post do |f|
 		f.title 		{ Faker::Lorem.sentence }
 		f.tagline		{ Faker::Lorem.sentence }
-		f.body { Faker::Lorem.paragraphs(10).join("  ") } 
+		f.body { generate_post_body } 
 		f.published	{ true }
 	end
 end
