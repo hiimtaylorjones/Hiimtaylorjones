e3e87a4c7cdf3632f70a6b59548fb528f41fac36
hiimtaylorjones <taylorjonessoftware@gmail.com>
2023-08-11 13:21:56 -0400

    patching admin errors; going back to previous format for admin page

--- app/controllers/basic_page_controller.rb
+++ app/controllers/basic_page_controller.rb
@@ -5,27 +5,11 @@ class BasicPageController < ApplicationController
     @posts = @posts.order('created_at DESC').paginate(:page => params[:page], :per_page => 1)
   end
 
-  def about
-    @page = Page.find_by(title: "About")
-    render layout: "page"
-  end
-
-  def writing
-    @page = Page.find_by(title: "Writing")
-    render layout: "page"
-  end
-
-  def speaking
-    @page = Page.find_by(title: "Speaking")
-    render layout: "page"
-  end
-
   def admin
     authenticate_admin!
     @admins = Admin.all
     @posts = Post.paginate(page: params[:page], per_page: 10).order('created_at DESC')
     @pages = Page.all
-    render layout: "page"
   end
 
 end
